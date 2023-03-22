bc5c22f2b1a96ede68bcdbc91b5cc175d64d4723
Aywah01 <rujhanmunawwaruzzaman@gmail.com>
2023-03-22 12:05:08 +0900

    Week 3

--- dy0322/py5.py
+++ dy0322/py5.py
@@ -4,10 +4,12 @@
 import random
 
 sum = 0
-num = int(print("짝수 하나를 입력하세요>>"))
+num = int(input("짝수 하나를 입력하세요>>"))
 i = 0
 for i  in range (0, num) : 
     i += 1
     if i % 2 == 0 :
         sum += i
+
+print("1부터 n까지의 짝수의 합 구하기 = %d" % sum)
 
