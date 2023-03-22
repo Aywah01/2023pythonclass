9d02a42cd5600cc23f7d6aa37410b80d2ea09541
Aywah01 <rujhanmunawwaruzzaman@gmail.com>
2023-03-22 12:01:40 +0900

    Week 3

---
+++ dy0322/py5.py
@@ -0,0 +1,13 @@
+# [6] 1부터 n까지의 짝수의 합을 구하는 코드를 while문과 for문으로 작성하시오
+# print("===1부터 n까지의 짝수의 합 구하기 =")
+
+import random
+
+sum = 0
+num = int(print("짝수 하나를 입력하세요>>"))
+i = 0
+for i  in range (0, num) : 
+    i += 1
+    if i % 2 == 0 :
+        sum += i
+
